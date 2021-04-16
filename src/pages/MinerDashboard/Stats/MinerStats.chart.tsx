49f4483a8656851e875bfbf8fc4dd2d10f0cf12c
Roman Nguyen <march08@outlook.com>
2021-04-16 22:23:24 +0200

    chart color contrast #78, chart redraw and some fixes #35

--- src/pages/MinerDashboard/Stats/MinerStats.chart.tsx
+++ src/pages/MinerDashboard/Stats/MinerStats.chart.tsx
@@ -31,130 +31,147 @@ export const StatsChart: React.FC<{
   const sharesChartRef = useRef<HTMLDivElement>(null);
   const reportedHashrateSeriesRef = useRef<HTMLDivElement>(null);
 
+  const [sharesData, setSharesData] = React.useState<
+    | {
+        date: Date;
+        validShares: number;
+        staleShares: number;
+        invalidShares: number;
+      }[]
+    | null
+  >();
+  const [hashrateData, setHashrateData] = React.useState<
+    | {
+        date: Date;
+        effectiveHashrate: number;
+        averageEffectiveHashrate: number;
+        reportedHashrate: number;
+      }[]
+    | null
+  >();
+
   const worker = useActiveSearchParamWorker();
   const appTheme = useAppTheme();
   useEffect(() => {
-    let hashrateChart = create('hashrate-chart', XYChart);
+    if (sharesData && hashrateData) {
+      let hashrateChart = create('hashrate-chart', XYChart);
 
-    hashrateChart.responsive.enabled = true;
-    hashrateChart.responsive.useDefault = false;
-    hashrateChart.responsive.rules.push(responsiveRule);
+      hashrateChart.responsive.enabled = true;
+      hashrateChart.responsive.useDefault = false;
+      hashrateChart.responsive.rules.push(responsiveRule);
 
-    hashrateChart.colors.list = [
-      color(appTheme === 'dark' ? '#aaa' : '#000000'),
-      color('#0069ff'),
-      color('#15cd72'),
-    ];
+      hashrateChart.colors.list = [
+        color(appTheme === 'dark' ? '#aaa' : '#000000'),
+        color('#0069ff'),
+        color('#15cd72'),
+      ];
 
-    let sharesChart = create('shares-chart', XYChart);
-    sharesChart.responsive.enabled = true;
-    sharesChart.responsive.useDefault = false;
-    sharesChart.responsive.rules.push(responsiveRule);
-    sharesChart.colors.list = [
-      color('#444444'),
-      color('#edb431'),
-      color('#0069ff'),
-    ];
+      let sharesChart = create('shares-chart', XYChart);
+      sharesChart.responsive.enabled = true;
+      sharesChart.responsive.useDefault = false;
+      sharesChart.responsive.rules.push(responsiveRule);
+      sharesChart.colors.list = [
+        color('#444444'),
+        color('#edb431'),
+        color('#0069ff'),
+      ];
 
-    var hashrateAxis = hashrateChart.yAxes.push(new ValueAxis());
-    hashrateAxis.numberFormatter = new NumberFormatter();
-    hashrateAxis.renderer.grid.template.disabled = true;
-    hashrateAxis.numberFormatter.numberFormat = '#.0 aH/s';
-    let dateAxis = hashrateChart.xAxes.push(new DateAxis());
-    dateAxis.renderer.grid.template.location = 0;
-    dateAxis.baseInterval = {
-      timeUnit: 'minute',
-      count: 1,
-    };
+      var hashrateAxis = hashrateChart.yAxes.push(new ValueAxis());
+      hashrateAxis.numberFormatter = new NumberFormatter();
+      hashrateAxis.renderer.grid.template.disabled = true;
+      hashrateAxis.numberFormatter.numberFormat = '#.0 aH/s';
+      let dateAxis = hashrateChart.xAxes.push(new DateAxis());
+      dateAxis.renderer.grid.template.location = 0;
+      dateAxis.baseInterval = {
+        timeUnit: 'minute',
+        count: 1,
+      };
 
-    let reportedHashrateSeries = hashrateChart.series.push(new LineSeries());
-    reportedHashrateSeries.dataFields.dateX = 'date';
-    reportedHashrateSeries.name = 'Reported Hashrate';
-    reportedHashrateSeries.yAxis = hashrateAxis;
-    reportedHashrateSeries.dataFields.valueY = 'reportedHashrate';
-    reportedHashrateSeries.tooltipText = `{name}: {valueY.value.formatNumber("#.00 aH/s")}`;
-    reportedHashrateSeries.strokeWidth = 3;
-    reportedHashrateSeries.smoothing = 'monotoneX';
-    // reportedHashrateSeries.monotoneX = 0.9;
-    // reportedHashrateSeries.monotoneY = 0.9;
+      let reportedHashrateSeries = hashrateChart.series.push(new LineSeries());
+      reportedHashrateSeries.dataFields.dateX = 'date';
+      reportedHashrateSeries.name = 'Reported Hashrate';
+      reportedHashrateSeries.yAxis = hashrateAxis;
+      reportedHashrateSeries.dataFields.valueY = 'reportedHashrate';
+      reportedHashrateSeries.tooltipText = `{name}: {valueY.value.formatNumber("#.00 aH/s")}`;
+      reportedHashrateSeries.strokeWidth = 3;
+      reportedHashrateSeries.smoothing = 'monotoneX';
+      // reportedHashrateSeries.monotoneX = 0.9;
+      // reportedHashrateSeries.monotoneY = 0.9;
 
-    /// @ts-ignore
-    reportedHashrateSeriesRef.current = reportedHashrateSeries;
+      let effectiveHashrateSeries = hashrateChart.series.push(new LineSeries());
+      effectiveHashrateSeries.dataFields.dateX = 'date';
+      effectiveHashrateSeries.name = 'Effective Hashrate';
+      effectiveHashrateSeries.yAxis = hashrateAxis;
+      effectiveHashrateSeries.dataFields.valueY = 'effectiveHashrate';
+      effectiveHashrateSeries.tooltipText = `{name}: {valueY.value.formatNumber("#.00 aH/s")}`;
+      effectiveHashrateSeries.strokeWidth = 3;
+      effectiveHashrateSeries.smoothing = 'monotoneX';
+      // effectiveHashrateSeries.monotoneX = 0.9;
+      // effectiveHashrateSeries.monotoneY = 0.9;
 
-    let effectiveHashrateSeries = hashrateChart.series.push(new LineSeries());
-    effectiveHashrateSeries.dataFields.dateX = 'date';
-    effectiveHashrateSeries.name = 'Effective Hashrate';
-    effectiveHashrateSeries.yAxis = hashrateAxis;
-    effectiveHashrateSeries.dataFields.valueY = 'effectiveHashrate';
-    effectiveHashrateSeries.tooltipText = `{name}: {valueY.value.formatNumber("#.00 aH/s")}`;
-    effectiveHashrateSeries.strokeWidth = 3;
-    effectiveHashrateSeries.smoothing = 'monotoneX';
-    // effectiveHashrateSeries.monotoneX = 0.9;
-    // effectiveHashrateSeries.monotoneY = 0.9;
+      let averageEffectiveHashrateSeries = hashrateChart.series.push(
+        new LineSeries()
+      );
+      averageEffectiveHashrateSeries.dataFields.dateX = 'date';
+      averageEffectiveHashrateSeries.name = 'Average Effective Hashrate';
+      averageEffectiveHashrateSeries.yAxis = hashrateAxis;
+      averageEffectiveHashrateSeries.dataFields.valueY =
+        'averageEffectiveHashrate';
+      averageEffectiveHashrateSeries.tooltipText = `{name}: {valueY.value.formatNumber("#.00 aH/s")}`;
+      averageEffectiveHashrateSeries.strokeWidth = 3;
+      averageEffectiveHashrateSeries.smoothing = 'monotoneX';
+      // averageEffectiveHashrateSeries.monotoneX = 0.9;
+      // averageEffectiveHashrateSeries.monotoneY = 0.9;
 
-    let averageEffectiveHashrateSeries = hashrateChart.series.push(
-      new LineSeries()
-    );
-    averageEffectiveHashrateSeries.dataFields.dateX = 'date';
-    averageEffectiveHashrateSeries.name = 'Average Effective Hashrate';
-    averageEffectiveHashrateSeries.yAxis = hashrateAxis;
-    averageEffectiveHashrateSeries.dataFields.valueY =
-      'averageEffectiveHashrate';
-    averageEffectiveHashrateSeries.tooltipText = `{name}: {valueY.value.formatNumber("#.00 aH/s")}`;
-    averageEffectiveHashrateSeries.strokeWidth = 3;
-    averageEffectiveHashrateSeries.smoothing = 'monotoneX';
-    // averageEffectiveHashrateSeries.monotoneX = 0.9;
-    // averageEffectiveHashrateSeries.monotoneY = 0.9;
+      hashrateChart.cursor = new XYCursor();
+      hashrateChart.legend = new Legend();
 
-    hashrateChart.cursor = new XYCursor();
-    hashrateChart.legend = new Legend();
+      var sharesAxis = sharesChart.yAxes.push(new ValueAxis());
+      sharesAxis.numberFormatter = new NumberFormatter();
+      sharesAxis.renderer.grid.template.disabled = true;
+      sharesAxis.numberFormatter.numberFormat = '#';
+      let dateAxisShares = sharesChart.xAxes.push(new DateAxis());
+      dateAxisShares.renderer.grid.template.location = 0;
+      dateAxisShares.baseInterval = {
+        timeUnit: 'minute',
+        count: 1,
+      };
 
-    var sharesAxis = sharesChart.yAxes.push(new ValueAxis());
-    sharesAxis.numberFormatter = new NumberFormatter();
-    sharesAxis.renderer.grid.template.disabled = true;
-    sharesAxis.numberFormatter.numberFormat = '#';
-    let dateAxisShares = sharesChart.xAxes.push(new DateAxis());
-    dateAxisShares.renderer.grid.template.location = 0;
-    dateAxisShares.baseInterval = {
-      timeUnit: 'minute',
-      count: 1,
-    };
+      const shares = {
+        invalidShares: 'Invalid Shares',
+        staleShares: 'Stale Shares',
+        validShares: 'Valid Shares',
+      };
 
-    const shares = {
-      invalidShares: 'Invalid Shares',
-      staleShares: 'Stale Shares',
-      validShares: 'Valid Shares',
-    };
+      for (const share in shares) {
+        let shareSeries = sharesChart.series.push(new ColumnSeries());
+        shareSeries.dataFields.dateX = 'date';
 
-    for (const share in shares) {
-      let shareSeries = sharesChart.series.push(new ColumnSeries());
-      shareSeries.dataFields.dateX = 'date';
+        /// @ts-ignore
+        shareSeries.name = shares[share];
+        shareSeries.yAxis = sharesAxis;
+        shareSeries.dataFields.valueY = share;
+        shareSeries.tooltipText = `{name}: {valueY.value.formatNumber("#")}`;
+        shareSeries.strokeWidth = 3;
+        // shareSeries.smoothing = "monotoneX";
+        // shareSeries.monotoneX = 0.9;
+        // shareSeries.monotoneY = 0.9;
+        shareSeries.stacked = true;
+      }
 
-      /// @ts-ignore
-      shareSeries.name = shares[share];
-      shareSeries.yAxis = sharesAxis;
-      shareSeries.dataFields.valueY = share;
-      shareSeries.tooltipText = `{name}: {valueY.value.formatNumber("#")}`;
-      shareSeries.strokeWidth = 3;
-      // shareSeries.smoothing = "monotoneX";
-      // shareSeries.monotoneX = 0.9;
-      // shareSeries.monotoneY = 0.9;
-      shareSeries.stacked = true;
+      sharesChart.cursor = new XYCursor();
+      sharesChart.legend = new Legend();
+      sharesChart.legend.reverseOrder = true;
+
+      sharesChart.data = sharesData.reverse();
+      hashrateChart.data = hashrateData.reverse();
+
+      return () => {
+        hashrateChart.dispose();
+        sharesChart.dispose();
+      };
     }
-
-    sharesChart.cursor = new XYCursor();
-    sharesChart.legend = new Legend();
-    sharesChart.legend.reverseOrder = true;
-    // @ts-ignore
-    hashrateChartRef.current = hashrateChart;
-    // @ts-ignore
-    sharesChartRef.current = sharesChart;
-
-    return () => {
-      hashrateChart.dispose();
-      sharesChart.dispose();
-    };
-  }, [appTheme]);
+  }, [appTheme, sharesData, hashrateData]);
 
   useEffect(() => {
     if (props.coinTicker === null) return;
@@ -177,24 +194,16 @@ export const StatsChart: React.FC<{
     }).then((resp) => {
       if (resp === null) {
         setNoDataAvailable(true);
-        /// @ts-ignore
-        hashrateChartRef.current.dispose();
-        /// @ts-ignore
-        sharesChartRef.current.dispose();
+        setSharesData(null);
+        setHashrateData(null);
         return;
       }
       setNoDataAvailable(false);
       var hashrateChartData = [];
       var sharesChartData = [];
 
-      /// @ts-ignore
-      reportedHashrateSeriesRef.current.show();
-
-      var reportedHashrateEnabled = false;
-
       for (var i = 0; i < resp.length; i++) {
         const item = resp[i];
-        if (item.reportedHashrate > 0) reportedHashrateEnabled = true;
         hashrateChartData.push({
           date: new Date(item.timestamp * 1000),
           effectiveHashrate: item.effectiveHashrate,
@@ -210,15 +219,8 @@ export const StatsChart: React.FC<{
         });
       }
 
-      if (!reportedHashrateEnabled) {
-        /// @ts-ignore
-        reportedHashrateSeriesRef.current.hide();
-      }
-
-      /// @ts-ignore
-      sharesChartRef.current.data = sharesChartData.reverse();
-      /// @ts-ignore
-      hashrateChartRef.current.data = hashrateChartData.reverse();
+      setSharesData(sharesChartData);
+      setHashrateData(hashrateChartData);
     });
   }, [props.coinTicker, props.address, worker]);
 
