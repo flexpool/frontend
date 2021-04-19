4417e0ea3400810c4df371d3dafa617bf7c66a76
Roman Nguyen <march08@outlook.com>
2021-04-19 20:19:16 +1000

    display full numbers when not too big resolve #99

--- src/utils/si.utils.ts
+++ src/utils/si.utils.ts
@@ -1,13 +1,34 @@
 const siSymbols = ['', 'k', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y'];
 
-export const formatSi = (value?: number, unit: string = '', decimals = 1) => {
+export const formatSi = (
+  value?: number,
+  unit: string = '',
+  opts?: {
+    decimals?: number;
+    /**
+     * default is 1000
+     * 1000: 23402 => 23.4 k, 1504 => 1.4 k
+     * 10000: 23402 => 23.4 k, 1504 => 1,504
+     * 100000: 23402 => 23,402, 1504 => 1,504
+     */
+    shortenAbove?: number;
+  }
+) => {
+  const options = {
+    decimals: 1,
+    shortenAbove: 1000,
+    ...opts,
+  };
+
+  let decimals = options.decimals;
+
   if (typeof value === 'undefined') {
     return null;
   }
 
   var siN = 0;
 
-  while (value >= 1000) {
+  while (value >= options.shortenAbove) {
     if (siN >= siSymbols.length) {
       break;
     }
@@ -17,7 +38,7 @@ export const formatSi = (value?: number, unit: string = '', decimals = 1) => {
 
   if (value < 10 && decimals === 1) decimals = 2;
 
-  return `${
+  return `${Intl.NumberFormat().format(
     Math.round(value * Math.pow(10, decimals)) / Math.pow(10, decimals)
-  } ${siSymbols[siN]}${unit}`;
+  )} ${siSymbols[siN]}${unit}`;
 };
