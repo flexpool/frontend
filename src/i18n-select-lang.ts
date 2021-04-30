bfcbfdcd77ea42fc3799f7e8b5e5ec4691e94d9b
Roman Nguyen <march08@outlook.com>
2021-04-30 10:15:24 +0200

    add lang config to root

---
+++ src/i18n-select-lang.ts
@@ -0,0 +1,42 @@
+export const availableLangs = [
+  {
+    code: 'en-US',
+    title: 'English (US)',
+    flag: 'us',
+  },
+  {
+    code: 'en-GB',
+    title: 'English (UK)',
+    flag: 'gb',
+  },
+  {
+    code: 'ru',
+    title: 'Русский',
+    flag: 'ru',
+  },
+  {
+    code: 'pt-PT',
+    title: 'Português',
+    flag: 'pt',
+  },
+  {
+    code: 'de',
+    title: 'Deutsch',
+    flag: 'de',
+  },
+  {
+    code: 'es-ES',
+    title: 'Español',
+    flag: 'es',
+  },
+  {
+    code: 'zh-CN',
+    title: '中文',
+    flag: 'cn',
+  },
+  {
+    code: 'cs',
+    title: 'Česky',
+    flag: 'cz',
+  },
+];
