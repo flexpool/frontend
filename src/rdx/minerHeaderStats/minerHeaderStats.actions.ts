9f72829a1318716a6eba7bbbe72210944120da42
Roman Nguyen <march08@outlook.com>
2021-05-05 12:45:30 +0200

    use new header stat endpoints

--- src/rdx/minerHeaderStats/minerHeaderStats.actions.ts
+++ src/rdx/minerHeaderStats/minerHeaderStats.actions.ts
@@ -1,3 +1,4 @@
+import { ApiMinerHeaderStats } from 'src/types/Miner.types';
 import { fetchApi } from 'src/utils/fetchApi';
 
 export const minerHeaderStatsGet = (
@@ -5,14 +6,41 @@ export const minerHeaderStatsGet = (
   address: string,
   counterTicker: string
 ) => {
+  const query = {
+    coin,
+    address,
+    countervalue: counterTicker,
+  };
   return {
     type: '@minerHeaderStats/GET',
-    payload: fetchApi('/miner/headerStats', {
-      query: {
-        coin,
-        address,
-        countervalue: counterTicker,
-      },
+    payload: Promise.all([
+      fetchApi<ApiMinerHeaderStats>('/miner/headerStats', {
+        query: {
+          coin,
+          address,
+          countervalue: counterTicker,
+        },
+      }),
+      fetchApi<{ workersOnline: number; workersOffline: number }>(
+        '/miner/workerCount',
+        { query }
+      ),
+      fetchApi<{ balance: number; balanceCounterValue: number }>(
+        '/miner/balance',
+        { query }
+      ),
+      fetchApi<number>('/miner/roundShare', { query }),
+    ]).then((res) => {
+      return {
+        ...res[0],
+        ...res[1],
+        ...res[2],
+        ...(typeof res[3] === 'number'
+          ? {
+              roundShare: res[3],
+            }
+          : res[3]),
+      };
     }),
   };
 };
