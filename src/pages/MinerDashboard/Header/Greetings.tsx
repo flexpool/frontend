a1c64b7f9651b787746d464cb632b96b2a1538ff
hendoban <84650808+hendoban@users.noreply.github.com>
2021-05-27 11:50:06 -0400

    Add auto update toggle

--- src/pages/MinerDashboard/Header/Greetings.tsx
+++ src/pages/MinerDashboard/Header/Greetings.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { BiRefresh } from 'react-icons/bi';
 import { Button } from 'src/components/Button';
 import { useActiveCoin } from 'src/rdx/localSettings/localSettings.hooks';
 import { useReduxState } from 'src/rdx/useReduxState';
 import { useLocalizedSiFormatter } from 'src/utils/si.utils';
 import styled from 'styled-components';
+import { useLocalStorageState } from 'src/hooks/useLocalStorageState';
+import { FaToggleOff, FaToggleOn } from 'react-icons/fa';
+import { UIVariant } from 'styled-components/macro';
 
 export function getGreeting() {
   const hours = new Date().getHours();
@@ -30,12 +32,37 @@ const Wrap = styled.div`
   justify-content: space-between;
   align-items: center;
 `;
+const AutoUpdateWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  font-size: 1.1rem;
+  font-weight: 600;
+  color: var(--text-secondary);
+  align-self: flex-start;
+`;
 
-const RefreshButton = styled(Button)`
-  font-size: 1.5rem;
-  @media screen and (max-width: 768px) {
-    display: none;
-  }
+const ActiveToggle = styled(FaToggleOn)`
+  color: var(--primary);
+  margin-left: 0.5rem;
+  height: 22px;
+  width: 80px;
+`;
+
+const InactiveToggle = styled(FaToggleOff)`
+  color: var(--text-secondary);
+  margin-left: 0.5rem;
+  height: 22px;
+  width: 80px;
+`;
+
+const ToggleWrapperButton = styled(Button)`
+  padding: 0 0 0 0;
+  border: none;
+  cursor: pointer;
+  overflow: hidden;
+  outline: none;
+  height: 20px;
+  width: 40px;
 `;
 
 export const HeaderGreetings: React.FC<{ onRefresh: () => void }> = ({
@@ -48,6 +75,25 @@ export const HeaderGreetings: React.FC<{ onRefresh: () => void }> = ({
   const { t } = useTranslation('dashboard');
 
   const data = minerHeaderStatsState.data;
+  const [counter, setCounter] = React.useState(60);
+
+  const [autoRefresh, setAutoRefresh] = useLocalStorageState<'auto' | 'manual'>(
+    'auto_refresh_status',
+    'auto'
+  );
+
+  const autoRefreshMethod = () => {
+    if (autoRefresh === 'auto') {
+      onRefresh();
+      setCounter(60);
+    }
+  };
+
+  React.useEffect(() => {
+    counter > 0 && autoRefresh === 'auto'
+      ? setTimeout(() => setCounter(counter - 1), 1000)
+      : autoRefreshMethod();
+  }, [counter]);
 
   const greetingId = React.useMemo(() => {
     return getGreeting();
@@ -77,11 +123,28 @@ export const HeaderGreetings: React.FC<{ onRefresh: () => void }> = ({
           })}
         </span>
       </div>
-      <div>
-        <RefreshButton size="sm" shape="square" onClick={onRefresh}>
-          <BiRefresh />
-        </RefreshButton>
-      </div>
+      <AutoUpdateWrapper>
+        <div>
+          {autoRefresh === 'auto' ? (
+            <span>
+              {t('header.update_in')} {counter}
+            </span>
+          ) : (
+            <span>{t('header.auto_update')}</span>
+          )}
+        </div>
+        <div>
+          <ToggleWrapperButton
+            variant="transparent"
+            onClick={() => {
+              setAutoRefresh(autoRefresh === 'auto' ? 'manual' : 'auto');
+              setCounter(59);
+            }}
+          >
+            {autoRefresh === 'auto' ? <ActiveToggle /> : <InactiveToggle />}
+          </ToggleWrapperButton>
+        </div>
+      </AutoUpdateWrapper>
     </Wrap>
   );
 };
