d29645036865cb15a6616814e1bcdca012dbdeec
hendoban <84650808+hendoban@users.noreply.github.com>
2021-05-28 07:22:37 -0400

    v1 url support

---
+++ src/pages/V1Urlsupport/MinersOldUrlSupport.page.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Redirect, RouteComponentProps } from 'react-router';
+import HandleLegacyLangVariable from './helpers/LanguageSelectionHelper';
+
+export const MinersOldUrlSupportPage: React.FC<
+  RouteComponentProps<{ lang: string }>
+> = ({
+  match: {
+    params: { lang },
+  },
+}) => {
+  HandleLegacyLangVariable(lang);
+  return <Redirect to={`/miners`} />;
+};
+
+export default MinersOldUrlSupportPage;
