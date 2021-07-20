e2b0fe80647eb8ec36c94986b1b792b0fb0c8f80
Pieter Montoulieu <pieter@montoulieu.dev>
2021-07-20 14:02:48 -0600

    Migrated Miner Rewards to redux store to fix rewards tab not showing after coin change

---
+++ src/rdx/minerRewards/minerRewards.actions.ts
@@ -0,0 +1,15 @@
+import { ApiMinerReward } from 'src/types/Miner.types';
+import { fetchApi } from 'src/utils/fetchApi';
+
+export const minerRewardsGet = (
+  coin: string,
+  address: string,
+  countervalue: string
+) => {
+  return {
+    type: '@minerRewards/GET',
+    payload: fetchApi('/miner/rewards', {
+      query: { coin, address, countervalue },
+    }),
+  };
+};
