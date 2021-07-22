17c2258c54301f7865554314b527bb26342f1dbb
Pieter Montoulieu <pieter@montoulieu.dev>
2021-07-22 14:53:29 -0600

    Integrating Google Fonts and other analytics head tags

--- pages/_document.js
+++ pages/_document.js
@@ -17,7 +17,17 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>{this.props.styleTags}</Head>
+        <Head>
+          <link rel="preconnect" href="https://static.flexpool.io" />
+          <link rel="preconnect" href="https://js.intercomcdn.com" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://xtwj9bs7n2j9.statuspage.io" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;400;500;600;700;800&family=Roboto+Mono:wght@400;500&display=swap"
+            rel="stylesheet"
+          />
+          {this.props.styleTags}
+        </Head>
         <body>
           <Main />
           <NextScript />
