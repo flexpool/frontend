10fe17ee277999eece6bfa2f12a1cac69bcb81d9
Pieter Montoulieu <pieter@montoulieu.dev>
2021-07-22 12:44:32 -0600

    Next config and package updates

--- next.config.js
+++ next.config.js
@@ -1,13 +1,19 @@
 const { i18n } = require('./next-i18next-config.js');
 
-module.exports = {
-  i18n,
-};
-
 const withTM = require('next-transpile-modules')([
   '@amcharts/amcharts4/core',
   '@amcharts/amcharts4/charts',
   '@amcharts/amcharts4/themes/animated',
 ]);
 
-module.exports = withTM();
+module.exports = withTM({
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: 'frontmatter-markdown-loader',
+    });
+
+    return config;
+  },
+  i18n,
+});
