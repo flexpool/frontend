0925db98c502960f3c79d91cc8280e12ad99ef11
Pieter Montoulieu <pieter@montoulieu.dev>
2021-07-26 23:30:15 -0600

    Testing fix for prod routes on get started

--- src/pages/GetStarted/CoinList.page.tsx
+++ src/pages/GetStarted/CoinList.page.tsx
@@ -86,7 +86,8 @@ export const MineableCoinList: React.FC = () => {
                   {poolHw.map((itemHw) => (
                     <Link
                       key={itemHw.key}
-                      href={`/get-started/${item.ticker}/${itemHw.key}`}
+                      href="/get-started/:ticker/:hw"
+                      as={`/get-started/${item.ticker}/${itemHw.key}`}
                       passHref
                     >
                       <Button shape="block" size="sm" variant="primary">
