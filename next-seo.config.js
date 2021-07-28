b5aabdabd3e63e18fd2e10c573fe588f0cb63fc5
Pieter Montoulieu <pieter@montoulieu.dev>
2021-07-28 14:27:48 -0600

    Updates to open graph in SEO config

--- next-seo.config.js
+++ next-seo.config.js
@@ -4,11 +4,19 @@ const SEO = {
     type: 'website',
     locale: 'en_US',
     url: 'https://flexpool.io',
-    site_name: 'Flexpool',
+    site_name: 'Flexpool.io',
+    description:
+      'The mining pool that offers you the best mining experience. We build the future of mining pools here.',
+    images: [
+      {
+        url: 'https://flexpool.io/og/flexpool_og.jpg',
+        alt: 'Flexpool.io | Building the Future of Mining Pools ',
+      },
+    ],
   },
   twitter: {
-    handle: '@handle',
-    site: '@site',
+    handle: '@flexpool_io',
+    site: '@flexpool_io',
     cardType: 'summary_large_image',
   },
 };
