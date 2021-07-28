8c8cb502072f0d74d59b2e11ca53fc76c45adc59
Pieter Montoulieu <pieter@montoulieu.dev>
2021-07-27 20:38:48 -0600

    Fixes for styled-components and server side rendering

--- pages/_document.js
+++ pages/_document.js
@@ -2,16 +2,30 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
-  static getInitialProps({ renderPage }) {
+  static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
-    const page = renderPage(
-      (App) => (props) => sheet.collectStyles(<App {...props} />)
-    );
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
 
-    const styleTags = sheet.getStyleElement();
-
-    return { ...page, styleTags };
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 
   render() {
@@ -43,75 +57,8 @@ export default class MyDocument extends Document {
             rel="stylesheet"
             prefetch={false}
           />
-          {/* <style
-            dangerouslySetInnerHTML={{
-              __html: `
-              #__next > :not(.full-page-loader) {
-                animation: fade 1s normal forwards;
-                transition: 1s opacity;
-              }
-
-              .full-page-loader {
-                display: flex;
-                align-items: center;
-                flex-direction: column;
-                justify-content: center;
-                position: absolute;
-                top: 0;
-                right: 0;
-                left: 0;
-                bottom: 0;
-                z-index: 10;
-                animation: fade 1s reverse forwards;
-                transition: 1s opacity;
-              }
-
-              .full-page-loader > img {
-                height: 70px;
-                animation: 1.8s infinite heartbeat;
-                transition: 0.2s all;
-              }
-
-              @keyframes fade {
-                  from {
-                      opacity: 0;
-                  }
-                  to {
-                      opacity: 1;
-                  }
-              }
-
-              @keyframes heartbeat {
-                0% {
-                  transform: scale(1);
-                }
-                25% {
-                  transform: scale(1.15);
-                }
-                50% {
-                  transform: scale(1);
-                }
-                75% {
-                  transform: scale(1.15);
-                }
-                100% {
-                  transform: scale(1);
-                }
-              }
-          `,
-            }}
-          /> */}
-          {this.props.styleTags}
         </Head>
         <body>
-          {/* <div className="full-page-loader">
-            <img
-              width="200"
-              loading="lazy"
-              src="https://static.flexpool.io/assets/brand/icon.svg"
-              alt="Flexpool.io logo"
-            />
-          </div> */}
           <Main />
           <NextScript />
         </body>
