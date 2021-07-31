24439253c17363f4795c9983d3a5bf9ef0cfc12a
Pieter Montoulieu <pieter@montoulieu.dev>
2021-07-31 11:19:46 -0600

    Adding dummy service worker file to unregister previous apps service worker

---
+++ public/service-worker.js
@@ -0,0 +1,11 @@
+if (window.navigator && navigator.serviceWorker) {
+  navigator.serviceWorker.getRegistrations().then(function (registrations) {
+    for (let registration of registrations) {
+      if (registration.active) {
+        if (registration.active.scriptURL.endsWith('service-worker.js')) {
+          registration.unregister();
+        }
+      }
+    }
+  });
+}
