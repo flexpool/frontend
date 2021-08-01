0ffba01b95853f45ede039246d7cb663ecf68eb1
Pieter Montoulieu <pieter@montoulieu.dev>
2021-07-31 18:09:32 -0600

    Removing intercom from document template

--- pages/_document.js
+++ pages/_document.js
@@ -103,52 +103,8 @@ export default class MyDocument extends Document {
           />
 
           <Script
-            onLoad={() => {
-              window.intercomSettings = {
-                app_id: 'fdj6l8hj',
-              };
-            }}
-          />
-
-          <Script
             id="ze-snippet"
             src="https://static.zdassets.com/ekr/snippet.js?key=d3afb9ee-8238-4043-b961-08e6c726a8f0"
-          />
-
-          <Script
-            onLoad={() => {
-              (function () {
-                var w = window;
-                var ic = w.Intercom;
-                if (typeof ic === 'function') {
-                  ic('reattach_activator');
-                  ic('update', w.intercomSettings);
-                } else {
-                  var d = document;
-                  var i = function () {
-                    i.c(arguments);
-                  };
-                  i.q = [];
-                  i.c = function (args) {
-                    i.q.push(args);
-                  };
-                  w.Intercom = i;
-                  var l = function () {
-                    var s = d.createElement('script');
-                    s.type = 'text/javascript';
-                    s.async = true;
-                    s.src = 'https://widget.intercom.io/widget/fdj6l8hj';
-                    var x = d.getElementsByTagName('script')[0];
-                    x.parentNode.insertBefore(s, x);
-                  };
-                  if (w.attachEvent) {
-                    w.attachEvent('onload', l);
-                  } else {
-                    w.addEventListener('load', l, false);
-                  }
-                }
-              })();
-            }}
           />
         </Head>
         <body>
