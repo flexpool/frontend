8a24f8d12d6d325bca4536e9a79b4c55ca2ba1cb
Nick Zhang <nzhang.jiahui@gmail.com>
2021-10-05 15:23:43 +0800

    Add more platforms

--- components/guides/flexfarmer/FlexfarmerDownloadLink.tsx
+++ components/guides/flexfarmer/FlexfarmerDownloadLink.tsx
@@ -68,9 +68,14 @@ export const FlexfarmerDownloadLink: React.FC<{
   React.useEffect(() => {
     if (version === null) return;
     checksumState.start(fetch(link + '.sha256')).then((res) => {
-      res.text().then((text) => {
-        setChecksum(text);
-      });
+      if (res.status === 200) {
+        res.text().then((text) => {
+          setChecksum(text);
+        });
+      } else {
+        setChecksum('N/A');
+      }
+
       return res;
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -82,23 +87,29 @@ export const FlexfarmerDownloadLink: React.FC<{
         onClick={() => {
           router.push(link);
         }}
-        title={`${t('download')} FlexFarmer ${version} (${osName} ${info.name})`}
+        title={`${t('download')} FlexFarmer ${version} (${osName} ${
+          info.name
+        })`}
       >
-        <Img
-          src={`https://static.flexpool.io/assets/os/${os}.png`}
-          alt={`${osName} logo`}
-        />
-        {osName} {info.name}
+        {os !== 'others' && (
+          <Img
+            src={`https://static.flexpool.io/assets/os/${os}.png`}
+            alt={`${osName} logo`}
+          />
+        )}
+        {os !== 'others' && `${osName}`} {info.name}
       </FlexfarmerDownloadButton>
       <FlexfarmerDownloadInfoBox>
         <div>
-          Version {version} <span style={{ color: 'var(--text-tertiary)' }}>|</span> {os}/
+          Version {version}{' '}
+          <span style={{ color: 'var(--text-tertiary)' }}>|</span> {os}/
           {info.arch}
         </div>
       </FlexfarmerDownloadInfoBox>
       <FlexfarmerDownloadInfoBox>
         <div>
-          SHA-256 Checksum: <span style={{ color: 'var(--primary)' }}>{checksum}</span>
+          SHA-256 Checksum:{' '}
+          <span style={{ color: 'var(--primary)' }}>{checksum}</span>
         </div>
       </FlexfarmerDownloadInfoBox>
     </FlexfarmerDownloadLinkWrapper>
--- src/utils/oses.tsx
+++ src/utils/oses.tsx
@@ -1,7 +1,19 @@
 export const osList = {
-  linux: { label: 'Linux', logoURL: 'https://static.flexpool.io/assets/os/linux.png' },
+  linux: {
+    label: 'Linux',
+    logoURL: 'https://static.flexpool.io/assets/os/linux.png',
+  },
   windows: {
     label: 'Windows',
     logoURL: 'https://static.flexpool.io/assets/os/windows.png',
+  },
+  macOS: {
+    label: 'macOS',
+    logoURL:
+      'https://cdn2.iconfinder.com/data/icons/metro-uinvert-dock/256/OS_Apple.png',
+  },
+  others: {
+    label: 'Others',
+    logoURL: '',
   },
 };
