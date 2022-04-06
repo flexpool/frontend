4ef4809328a6af6c4808df60f84ce6979e453589
Nick Zhang <nzhang.jiahui@gmail.com>
2022-04-06 21:09:56 +0800

    Add lambo explainer i18n support

--- src/pages/Home/components/LamboExplainer/LamboExplainer.tsx
+++ src/pages/Home/components/LamboExplainer/LamboExplainer.tsx
@@ -1,7 +1,9 @@
+import { useTranslation } from 'next-i18next';
 import { useCounterTicker } from '@/rdx/localSettings/localSettings.hooks';
 
 const LamboExplainer = () => {
   const counterTicker = useCounterTicker();
+  const { t } = useTranslation('home');
 
   if (counterTicker !== 'lambo') return null;
 
@@ -14,9 +16,7 @@ const LamboExplainer = () => {
         margin: '20px',
       }}
     >
-      <p style={{ color: 'var(--text-tertiary)' }}>
-        Ł - LAMBO currency. mŁ - mLAMBO (microLAMBO), one 1000th of LAMBO.
-      </p>
+      <p style={{ color: 'var(--text-tertiary)' }}>{t('lambo_explainer')}</p>
     </div>
   );
 };
