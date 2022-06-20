d9ec20527a93991bd1da9763bf03199d92f7ef5b
Nick Zhang <nzhang.jiahui@gmail.com>
2022-06-20 20:55:36 +0800

    Improve globe arc performance

--- src/components/Globe/components/Arcs/Arcs.tsx
+++ src/components/Globe/components/Arcs/Arcs.tsx
@@ -1,35 +1,38 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Arc from '../Arc';
-import useInterval from '@/hooks/useInterval';
 import { SERVERS } from '../../constants';
 
+type Coordinate = { longitude: number; latitude: number };
+
+const ArcOne = [
+  {
+    longitude: SERVERS[0].longitude,
+    latitude: SERVERS[0].latitude,
+  },
+  {
+    longitude: SERVERS[7].longitude,
+    latitude: SERVERS[7].latitude,
+  },
+] as [Coordinate, Coordinate];
+
+const ArcTwo = [
+  {
+    longitude: SERVERS[5].longitude,
+    latitude: SERVERS[5].latitude,
+  },
+  {
+    longitude: SERVERS[1].longitude,
+    latitude: SERVERS[1].latitude,
+  },
+] as [Coordinate, Coordinate];
+
 const Arcs = () => {
-  const [arcs, setArcs] = useState<{ from: number; to: number }[]>([
-    { from: 0, to: 7 },
-    { from: 5, to: 1 },
-  ]);
-
-  useInterval(() => {
-    if (document && !document.hidden) {
-      const from = Math.floor(Math.random() * 9);
-      const to = Math.floor(Math.random() * 9);
-      setArcs((a) => a.concat([{ from, to }]));
-    }
-  }, 3000);
-
   return (
     <>
-      {arcs.map((arc, index) => (
-        <Arc
-          key={`${arc.from}-${arc.to}-${index}`}
-          fromLatitude={SERVERS[arc.from].latitude}
-          fromLongitude={SERVERS[arc.from].longitude}
-          toLatitude={SERVERS[arc.to].latitude}
-          toLongitude={SERVERS[arc.to].longitude}
-        />
-      ))}
+      <Arc delay={2} initial={ArcOne} />
+      <Arc delay={4} initial={ArcTwo} />
     </>
   );
 };
 
-export default Arcs;
+export default React.memo(Arcs);
