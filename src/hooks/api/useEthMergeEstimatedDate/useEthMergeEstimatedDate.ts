9868a7ce405b0e0664696340a0195847134d9961
Nick Zhang <nzhang.jiahui@gmail.com>
2022-09-02 14:38:26 +0800

    Add merge farewell

--- src/hooks/api/useEthMergeEstimatedDate/useEthMergeEstimatedDate.ts
+++ src/hooks/api/useEthMergeEstimatedDate/useEthMergeEstimatedDate.ts
@@ -1,9 +1,15 @@
-import { useQuery } from 'react-query';
+import { useQuery, UseQueryOptions } from 'react-query';
 import { fetchApi } from '@/utils/fetchApi';
 
-const useEthMergeEstimatedDate = () => {
-  return useQuery(['/poo/ethMergeEstimatedDate'], () =>
-    fetchApi<number>('/pool/ethMergeEstimatedDate')
+const useEthMergeEstimatedDate = (
+  options?: UseQueryOptions<number, any, any, any>
+) => {
+  return useQuery(
+    ['/poo/ethMergeEstimatedDate'],
+    () => fetchApi<number>('/pool/ethMergeEstimatedDate'),
+    {
+      ...options,
+    }
   );
 };
 
