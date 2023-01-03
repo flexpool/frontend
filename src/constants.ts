d165079443e5906efcf15a7a8890bf61c5594c2e
Nick Zhang <nzhang.jiahui@gmail.com>
2023-01-03 17:30:29 +0800

    Add currency detection

--- src/constants.ts
+++ src/constants.ts
@@ -6,3 +6,6 @@ export const REDDIT_LINK = 'https://www.reddit.com/r/flexpool';
 export const TWITTER_LINK = 'https://twitter.com/flexpool_io';
 export const MEDIUM_LINK = 'https://medium.com/flexpool/';
 export const GITHUB_LINK = 'https://github.com/flexpool';
+
+// Cookies
+export const COOKIES_PREFERENCE_CURRENCY = 'preference_currency';
