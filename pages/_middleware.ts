295c766c875de48c4536cbb41ba4095f4da0b3b6
Nick Zhang <nzhang.jiahui@gmail.com>
2023-10-27 18:26:50 +0800

    Test middleware

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -7,6 +7,63 @@ const BLOCKED_COUNTRY = 'RU';
 export function middleware(req: NextRequest) {
   const country = req.geo?.country || 'US';
 
+  if (process.env.SUNSET === 'true') {
+    // Return response with html content
+    return new Response(
+      `<!DOCTYPE html>
+      <html>
+        <head>
+          <meta charset="utf-8">
+          <title>Site is down for maintenance</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          <style>
+            body {
+              background-color: #f6f6f6;
+              color: #333;
+              font-family: sans-serif;
+              margin: 0;
+              padding: 0;
+            }
+            .container {
+              margin: 0 auto;
+              max-width: 600px;
+              padding: 20px;
+            }
+            .logo {
+              display: block;
+              margin: 0 auto;
+              max-width: 100%;
+            }
+            .title {
+              font-size: 24px;
+              font-weight: bold;
+              margin: 20px 0;
+              text-align: center;
+            }
+            .description {
+              font-size: 16px;
+              line-height: 1.5;
+              margin: 20px 0;
+              text-align: center;
+            }
+          </style>
+        </head>
+        <body>
+          <div class="container">
+            <img class="logo" src="/logo.png" alt="Logo">
+            <div class="title">Site is down for maintenance</div>
+            <div class="description">We are currently working on the site. We will be back soon.</div>
+          </div>
+        </body>
+      </html>`,
+      {
+        headers: {
+          'Content-Type': 'text/html',
+        },
+      }
+    );
+  }
+
   if (
     req.nextUrl.pathname !== '/region-not-available' &&
     country === BLOCKED_COUNTRY
