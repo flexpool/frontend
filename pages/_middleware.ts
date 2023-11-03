6307dc92be32e2f0ed9e57500775db12de4b2fb3
AlexSSD7 <alexandersadovskyi7@protonmail.com>
2023-11-03 14:29:16 +0000

    Update the closure notice

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -38,14 +38,15 @@ export async function middleware(req: NextRequest) {
       <h3>Notice of Closure</h3>
       <p>
         Flexpool.io was shut down on November 1st, 2023. Flexpool.io no longer
-        operates any mining/farming pool services. Any outstanding balances are
-        to be paid out shortly, within the week following the termination of
-        Flexpool.io's mining/farming services.
+        operates any mining/farming pool services. All outstanding balances have been paid out.
       </p>
       <p>
         To continue your mining/farming operations, you need to switch to
         another pool if you have not done it by the closure date as initially
         recommended.
+      </p>
+      <p>
+        Inquiries: <a href="mailto:hq@flexpool.io">hq@flexpool.io</a>
       </p>
     </div>
   </body>
